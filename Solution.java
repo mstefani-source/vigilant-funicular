4ff3dc94e379223ab07067afef97e94cb762dc0c
mstefani-source <ivanow.victor@live.ru>
2024-12-14 22:27:42 +0300

    10ms solution

--- Solution.java
+++ Solution.java
@@ -1,18 +1,38 @@
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+
 class Solution {
 
-    public static String reverseWords(String s) {
+    public String reverseWords(String s) {
 
         byte[] byteArray = s.getBytes();
+        ByteArrayInputStream bais = new ByteArrayInputStream(byteArray);
+        ArrayList<String> revStringArray = new ArrayList<>();
+        ArrayList<String> word = new ArrayList<>();
+        int currentLetter = bais.read();
 
-        for (int i = byteArray.length - 1; i >= 0; i--){
-            System.out.println(byteArray[i]);
-        }
-
-        return s;
+        while (currentLetter != -1) {
+            if (currentLetter == 32 && !word.isEmpty()) {
+                revStringArray.add(0, String.join("", word));
+                word.clear();
+            } else if (currentLetter != 32) {
+                String strAsciiTab = Character.toString((char) currentLetter);
+                word.add(strAsciiTab);
+            }
+            currentLetter = bais.read();
+        } 
+        if (!word.isEmpty()) revStringArray.add(0, String.join("", word));
+        
+        return String.join(" ", revStringArray);
     }
 
     public static void main(String[] args) {
 
-        System.out.print(reverseWords(" new  era "));
+        Solution sol = new Solution();
+
+        System.out.println(sol.reverseWords(" Hello   World "));
+        System.out.println(sol.reverseWords("Plan B"));
+        System.out.println(sol.reverseWords("the sky is blue"));
     }
 }
