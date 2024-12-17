5637d858d5a0211107c8563a4741774796d5dab4
mstefani-source <ivanow.victor@live.ru>
2024-12-18 01:02:47 +0300

    functional interfaces

--- Solution.java
+++ Solution.java
@@ -6,32 +6,35 @@ import java.util.function.Consumer;
 class Solution {
 
     public static String reverseWords(String s) {
-        
+
         String regex = "[\\s+]";
         String[] myArray = s.split(regex);
         ArrayList<String> revArray = new ArrayList<>();
         List<String> words = Arrays.asList(myArray);
 
-        Consumer <List<String>> trimConsumer = list -> {
+        Consumer<List<String>> trimConsumer = list -> {
             for (int x = 0; x < list.size() - 1; x++) {
-                list.set(x, list.get(x).trim()); 
+                list.set(x, list.get(x).trim());
             }
         };
 
-        Consumer <List<String>> fillupConsumer = list -> {
+        Consumer<List<String>> fillupConsumer = list -> {
             for (int x = list.size() - 1; x >= 0; x--) {
                 if (!list.get(x).isBlank()) {
                     revArray.add(list.get(x));
                 }
             }
         };
-        
+
         trimConsumer.andThen(fillupConsumer).accept(words);
         return String.join(" ", revArray);
     }
 
     public static void main(String[] args) {
 
+        String myStr = "I love cats. Cats are very easy to love. Cats are very popular.";
+        String regex = "(?i)cat";
+        System.out.println(myStr.replaceAll(regex, "dog"));
         System.out.print(reverseWords("  end of the day  "));
     }
 }
