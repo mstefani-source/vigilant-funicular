af3e6f0543e2fc5ee8478a1d454a694300c65d0f
mstefani-source <ivanow.victor@live.ru>
2024-12-17 22:59:53 +0300

    ver 3

--- Solution.java
+++ Solution.java
@@ -1,27 +1,37 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
 class Solution {
 
     public static String reverseWords(String s) {
-
+        
         String regex = "[\\s+]";
         String[] myArray = s.split(regex);
-
         ArrayList<String> revArray = new ArrayList<>();
-        for (int i = myArray.length - 1; i >= 0; i--) {
-            if (!"".equals(myArray[i])) {
-                revArray.add(myArray[i]);
+        List<String> words = Arrays.asList(myArray);
+
+        Consumer <List<String>> trimConsumer = list -> {
+            for (int x = 0; x < list.size() - 1; x++) {
+                list.set(x, list.get(x).trim()); 
             }
-        }
+        };
+
+        Consumer <List<String>> fillupConsumer = list -> {
+            for (int x = list.size() - 1; x >= 0; x--) {
+                if (!list.get(x).isBlank()) {
+                    revArray.add(list.get(x));
+                }
+            }
+        };
+        
+        trimConsumer.andThen(fillupConsumer).accept(words);
         return String.join(" ", revArray);
     }
 
     public static void main(String[] args) {
 
-        System.out.println(reverseWords(" new era "));
-        System.out.println(reverseWords(" Hello   World "));
-        System.out.println(reverseWords("Plan B"));
-        System.out.println(reverseWords("the sky is blue"));
-
+        System.out.print(reverseWords("  end of the day  "));
     }
 }
