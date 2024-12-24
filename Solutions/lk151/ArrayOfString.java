9e70d8b307de6c34f348c47389f4b71fcfd25457
Viktor <ya.hoshi@ya.ru>
2024-12-24 20:35:33 +0400

    add version for lk151

---
+++ Solutions/lk151/ArrayOfString.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class ArrayOfString {
+
+    public String reverseWords(String s) {
+
+        String regex = "[\\s+]";
+        String[] myArray = s.split(regex);
+        ArrayList<String> revArray = new ArrayList<>();
+
+        for (int i = myArray.length - 1; i >= 0; i--) {
+            if (!"".equals(myArray[i])) {
+                revArray.add(myArray[i]);
+            }
+        }
+        return String.join(" ", revArray);
+    }
+
+}
--- lkapp.java
+++ lkapp.java
@@ -5,9 +5,11 @@ public class lkapp {
     public static void main(String[] args) {
 
         Consumers lk151 = new Consumers();
-        ByteArray ba = new ByteArray();       
-        System.out.print(lk151.reverseWords("  end of the day  "));
-        System.out.print(ba.reverseWords("  hello world of java  "));
+        ByteArray ba = new ByteArray();
+        ArrayOfString aos = new ArrayOfString();      
+        System.out.print(lk151.reverseWords("  end   of the day  "));
+        System.out.print(ba.reverseWords("  hello world   of java  "));
+        System.out.print(aos.reverseWords("  hello world of  aos  "));
     
     }
 }
