4796a5b53080a54670ad2dbfc42948d478686de1
mstefani-source <ivanow.victor@live.ru>
2025-01-06 23:30:46 +0300

    working release

--- lkapp/Solutions/addtwonumbers/Solution2.java
+++ lkapp/Solutions/addtwonumbers/Solution2.java
@@ -8,35 +8,36 @@ public class Solution2 {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode result = new ListNode();
+        ListNode currentDigit = result;
         do {
-            // if (l1 == null) {
-            //     value1 = 0;
-            // } else {
-            //     value1 = l1.val;
-            //     l1 = l1.next;
-            // }
-            // if (l2 == null) {
-            //     value2 = 0;
-            // } else {
-            //     value2 = l2.val;
-            //     l2 = l2.next;
-            // }
-            value1 = l1 == null ? 0 : l1.val;
-            value2 = l2 == null ? 0 : l2.val;
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
-            
-            ListNode currentDigit = new ListNode();
-            currentDigit.next = result;
+            if (l1 == null) {
+                value1 = 0;
+            } else {
+                value1 = l1.val;
+                l1 = l1.next;
+            }
+            if (l2 == null) {
+                value2 = 0;
+            } else {
+                value2 = l2.val;
+                l2 = l2.next;
+            }
+
             int sum = value1 + value2 + placenum;
             placenum = sum > 9 ? 1 : 0;
+            
             currentDigit.val = sum % 10;
-            System.out.println("sum: " + sum + " reminder " + placenum + " val = " + currentDigit.val);
-            result = currentDigit;
+            if (l1 != null || l2 != null) { 
+                currentDigit.next = new ListNode(); 
+                currentDigit = currentDigit.next;
+            }
             
         } while (l1 != null || l2 != null);
-        result.val = placenum > 0 ? placenum: null;
+
+        if (placenum > 0) {
+            currentDigit.next = new ListNode(placenum);
+        } 
        
-        return result;
+        return result; 
     }
 }
