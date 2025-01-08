adbd44ff424735cecf8ebccafea67fb2c2e90cdf
mstefani-source <ivanow.victor@live.ru>
2025-01-08 23:34:17 +0300

    try to ref

--- lkapp/Solutions/addtwonumbers/Solution2.java
+++ lkapp/Solutions/addtwonumbers/Solution2.java
@@ -10,6 +10,7 @@ public class Solution2 {
 
         ListNode result = new ListNode();
         ListNode currentDigit = result;
+        
         do {
             if (l1 == null) {
                 value1 = 0;
@@ -17,6 +18,7 @@ public class Solution2 {
                 value1 = l1.val;
                 l1 = l1.next;
             }
+
             if (l2 == null) {
                 value2 = 0;
             } else {
@@ -26,19 +28,20 @@ public class Solution2 {
 
             int sum = value1 + value2 + placenum;
             placenum = sum > 9 ? 1 : 0;
-            currentDigit.val = sum % 10;
 
+            currentDigit.val = sum % 10;
             if (l1 != null || l2 != null) {
                 currentDigit.next = new ListNode();
-                currentDigit = currentDigit.next;
             }
+            currentDigit = currentDigit.next;
 
         } while (l1 != null || l2 != null);
 
         if (placenum > 0) {
             currentDigit.next = new ListNode(placenum);
-        } 
-       
-        return result; 
+        }
+
+        currentDigit = null;
+        return result;
     }
 }
