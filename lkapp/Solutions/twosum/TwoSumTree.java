00dc3bffeb5984db3d572f93e5bde2f5ed08034a
VIKTOR IVANOV <55892410+mstefani-source@users.noreply.github.com>
2025-01-11 17:33:35 +0300

    Add two numbers (#17)
    * init
    
    * first attempt
    
    * ternar (#10)
    
    Co-authored-by: Viktor <ya.hoshi@ya.ru>
    
    * Add two numbers fix (#11)
    
    * ternar
    
    * attemt 1
    
    ---------
    
    Co-authored-by: Viktor <ya.hoshi@ya.ru>
    
    * Add two numbers fix (#12)
    
    * ternar
    
    * attemt 1
    
    * attempt 2
    
    ---------
    
    Co-authored-by: Viktor <ya.hoshi@ya.ru>
    
    * Add two numbers fix (#13)
    
    * ternar
    
    * attemt 1
    
    * attempt 2
    
    * working release
    
    ---------
    
    Co-authored-by: Viktor <ya.hoshi@ya.ru>
    
    * Add two numbers fix (#14)
    
    * ternar
    
    * attemt 1
    
    * attempt 2
    
    * working release
    
    * refaching
    
    ---------
    
    Co-authored-by: Viktor <ya.hoshi@ya.ru>
    
    * Update README.md
    
    * Add two numbers fix (#15)
    
    * ternar
    
    * attemt 1
    
    * attempt 2
    
    * working release
    
    * refaching
    
    * try to ref
    
    ---------
    
    Co-authored-by: Viktor <ya.hoshi@ya.ru>
    
    * Add two numbers fix (#16)
    
    * ternar
    
    * attemt 1
    
    * attempt 2
    
    * working release
    
    * refaching
    
    * try to ref
    
    ---------
    
    Co-authored-by: Viktor <ya.hoshi@ya.ru>
    
    ---------
    
    Co-authored-by: Viktor <ya.hoshi@ya.ru>

---
+++ lkapp/Solutions/twosum/TwoSumTree.java
@@ -0,0 +1,16 @@
+package lkapp.Solutions.twosum;
+
+import java.util.TreeSet;
+
+public class TwoSumTree {
+
+    int[] result = {0,0};
+
+    public int[] twoSum (int[] nums, int target) {
+        TreeSet <String> treeSet = new TreeSet<>();
+        treeSet.add("C");
+        treeSet.add("C");
+        System.out.println(treeSet); 
+        return result;
+    }
+}
--- lkapp/lkapp.java
+++ lkapp/lkapp.java
@@ -1,5 +1,7 @@
 package lkapp;
 
+import lkapp.Solutions.addtwonumbers.ListNode;
+import lkapp.Solutions.addtwonumbers.Solution2;
 import lkapp.Solutions.reversewordsinastring.*;
 import lkapp.Solutions.twosum.TwoSumHM;
 
@@ -13,14 +15,38 @@ public class lkapp {
         TwoSumHM tshm = new TwoSumHM(); 
         int[] nums = {3,3};
 
-        System.out.println(lk151.reverseWords("  end   of the day  "));
-        System.out.println(ba.reverseWords("  hello world   of java  "));
-        System.out.println(aos.reverseWords("  hello world of  aos  "));
-        int[] indexes = tshm.twoSum(nums, 6);
+        // System.out.println(lk151.reverseWords("  end   of the day  "));
+        // System.out.println(ba.reverseWords("  hello world   of java  "));
+        // System.out.println(aos.reverseWords("  hello world of  haos  "));
+        // int[] indexes = tshm.twoSum(nums, 6);
 
-        for (int i = 0; i < indexes.length; i++) {
-            System.out.println(indexes[i]);
-        }
+        // for (int i = 0; i < indexes.length; i++) {
+        //     System.out.println(indexes[i]);
+        // }
+
+        ListNode res;
+        Solution2 sol2 = new Solution2();
         
+        ListNode l1 = new ListNode(2);
+        ListNode l1d2 = new ListNode(4);
+        ListNode l1d1 = new ListNode(3);
+
+        l1.next = l1d2;
+        l1d2.next = l1d1;
+
+        ListNode l2 = new ListNode(5);
+        ListNode l2d2 = new ListNode(6);
+        ListNode l2d1 = new ListNode(4);
+
+        l2.next = l2d2;
+        l2d2.next = l2d1;
+
+        res = sol2.addTwoNumbers(l1, l2);
+
+        while (res != null){
+            System.out.println("res: " + res.val);
+            res = res.next;
+        }
+
     }
 }
