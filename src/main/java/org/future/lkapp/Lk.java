980400b85dcda7f8dc2320f09cf8f77f74fa0ee5
mstefani-source <ivanow.victor@live.ru>
2025-02-14 23:53:46 +0300

    first version

--- src/main/java/org/future/lkapp/Lk.java
+++ src/main/java/org/future/lkapp/Lk.java
@@ -7,6 +7,7 @@ import org.future.lkapp.Solutions.reversewordsinastring.*;
 import org.future.lkapp.Solutions.twosum.*;
 import org.future.lkapp.Solutions.addtwonumbers.*;
 import org.future.lkapp.Solutions.lengthoflongestsubstring.*;
+import org.future.lkapp.Solutions.lps.LongestPalindromicSubstring;
 import org.future.lkapp.Solutions.zigzag.*;
 
 /**
@@ -16,62 +17,66 @@ import org.future.lkapp.Solutions.zigzag.*;
 public class Lk {
 
     public static void main(String[] args) {
-        ReverseWordsConsumers lk151 = new ReverseWordsConsumers();
-        ReverseWordsByteArrayImputStream ba = new ReverseWordsByteArrayImputStream();
-        ReverseWordsArray aos = new ReverseWordsArray();
-        TwoSumHM tshm = new TwoSumHM();
-        int[] nums = { 3, 3 };
+        LongestPalindromicSubstring lps = new LongestPalindromicSubstring();
 
-        DayOfWeek.findByLabel("Su");
+        System.out.println("hey = " + lps.longestPalindrome("babad"));
 
-        ListNode res;
-        Solution2 sol2 = new Solution2();
+        // ReverseWordsConsumers lk151 = new ReverseWordsConsumers();
+        // ReverseWordsByteArrayImputStream ba = new ReverseWordsByteArrayImputStream();
+        // ReverseWordsArray aos = new ReverseWordsArray();
+        // TwoSumHM tshm = new TwoSumHM();
+        // int[] nums = { 3, 3 };
 
-        ListNode l1 = new ListNode(2);
-        ListNode l1d2 = new ListNode(4);
-        ListNode l1d1 = new ListNode(3);
+        // DayOfWeek.findByLabel("Su");
 
-        l1.next = l1d2;
-        l1d2.next = l1d1;
+        // ListNode res;
+        // Solution2 sol2 = new Solution2();
 
-        ListNode l2 = new ListNode(5);
-        ListNode l2d2 = new ListNode(6);
-        ListNode l2d1 = new ListNode(4);
+        // ListNode l1 = new ListNode(2);
+        // ListNode l1d2 = new ListNode(4);
+        // ListNode l1d1 = new ListNode(3);
 
-        l2.next = l2d2;
-        l2d2.next = l2d1;
+        // l1.next = l1d2;
+        // l1d2.next = l1d1;
 
-        res = sol2.addTwoNumbers(l1, l2);
+        // ListNode l2 = new ListNode(5);
+        // ListNode l2d2 = new ListNode(6);
+        // ListNode l2d1 = new ListNode(4);
 
-        LongestSubString str = new LongestSubString();
+        // l2.next = l2d2;
+        // l2d2.next = l2d1;
 
-        System.out.println("longest String: " + str.lengthOfLongestSubstring("pwwkew"));
-        System.out.println("longest String: " + str.lengthOfLongestSubstring("bbbbbb"));
-        System.out.println("longest String: " + str.lengthOfLongestSubstring("aab"));
-        System.out.println("longest String: " + str.lengthOfLongestSubstring("dvdf"));
-        System.out.println("longest String: " + str.lengthOfLongestSubstring("advsdw"));
+        // res = sol2.addTwoNumbers(l1, l2);
 
-        ZigZag zig = new ZigZag();
-        Map<Integer, String> testcases = Map.of(
-                1, "{'PAYPALISHIRING', 3}",
-                2, "{'PAYPALISHIRING', 4}",
-                3, "{'A', 3}",
-                4, "{'PAYP', 4}");
+        // LongestSubString str = new LongestSubString();
 
-        ZigZag zigZag = new ZigZag();
+        // System.out.println("longest String: " + str.lengthOfLongestSubstring("pwwkew"));
+        // System.out.println("longest String: " + str.lengthOfLongestSubstring("bbbbbb"));
+        // System.out.println("longest String: " + str.lengthOfLongestSubstring("aab"));
+        // System.out.println("longest String: " + str.lengthOfLongestSubstring("dvdf"));
+        // System.out.println("longest String: " + str.lengthOfLongestSubstring("advsdw"));
 
-        System.out.print("waiting for PAHNAPLSIIGYIR got ");
-        System.out.println(zigZag.convert("PAYPALISHIRING", 3) + " "
-                + zigZag.convert("PAYPALISHIRING", 3).equals("PAHNAPLSIIGYIR"));
-        System.out.print("waiting for PINALSIGYAHRPI got ");
-        System.out.println(zigZag.convert("PAYPALISHIRING", 4) + " "
-                + zigZag.convert("PAYPALISHIRING", 4).equals("PINALSIGYAHRPI"));
-        System.out.println(zigZag.convert("PAYP", 2) + " " + zigZag.convert("PAYP", 2).equals("PYAP"));
-        System.out.println(zigZag.convert("PAY", 2) + " " + zigZag.convert("PAY", 2).equals("PYA"));
-        System.out.println(
-                zigZag.convert("PAYPAL", 2) + " <-> " + "PYAAPL " + zigZag.convert("PAYPAL", 2).equals("PYAAPL"));
-        System.out.println(
-                zigZag.convert("PAYPAL", 3) + " <-> " + "PAAPLY " + zigZag.convert("PAYPAL", 3).equals("PAAPLY"));
-    }
+        // ZigZag zig = new ZigZag();
+        // Map<Integer, String> testcases = Map.of(
+        //         1, "{'PAYPALISHIRING', 3}",
+        //         2, "{'PAYPALISHIRING', 4}",
+        //         3, "{'A', 3}",
+        //         4, "{'PAYP', 4}");
 
+        // ZigZag zigZag = new ZigZag();
+
+        // System.out.print("waiting for PAHNAPLSIIGYIR got ");
+        // System.out.println(zigZag.convert("PAYPALISHIRING", 3) + " "
+        //         + zigZag.convert("PAYPALISHIRING", 3).equals("PAHNAPLSIIGYIR"));
+        // System.out.print("waiting for PINALSIGYAHRPI got ");
+        // System.out.println(zigZag.convert("PAYPALISHIRING", 4) + " "
+        //         + zigZag.convert("PAYPALISHIRING", 4).equals("PINALSIGYAHRPI"));
+        // System.out.println(zigZag.convert("PAYP", 2) + " " + zigZag.convert("PAYP", 2).equals("PYAP"));
+        // System.out.println(zigZag.convert("PAY", 2) + " " + zigZag.convert("PAY", 2).equals("PYA"));
+        // System.out.println(
+        //         zigZag.convert("PAYPAL", 2) + " <-> " + "PYAAPL " + zigZag.convert("PAYPAL", 2).equals("PYAAPL"));
+        // System.out.println(
+        //         zigZag.convert("PAYPAL", 3) + " <-> " + "PAAPLY " + zigZag.convert("PAYPAL", 3).equals("PAAPLY"));
+
+        }
 }
--- src/main/java/org/future/lkapp/Solutions/lps/LongestPalindromicSubstring.java
+++ src/main/java/org/future/lkapp/Solutions/lps/LongestPalindromicSubstring.java
@@ -10,19 +10,47 @@ public class LongestPalindromicSubstring {
             return result;
         }
         Set<String> setOfPalindromes = new HashSet<>();
-        
-        return result;
+        if (isPalindrome(s))
+            result = s;
+        int leftIndex = 0;
+        while (leftIndex < s.length() - 1) {
+            for (int i = leftIndex + 1; i < s.length(); i++) {
+                if (isPalindrome(s.substring(leftIndex, i))){
+                    if (!setOfPalindromes.contains(s.substring(leftIndex, i))){
+                        setOfPalindromes.add(s.substring(leftIndex,i));
+                    }
+                }
+            }
+            leftIndex++;
+        }
+        return findLongestString(setOfPalindromes);
     }
 
     private boolean isPalindrome(String s) {
         if (s.length() == 1) {
             return true;
         }
-        if (s.charAt(1) == s.charAt(s.length() - 1)) {
+        if (s.charAt(0) == s.charAt(s.length() - 1)) {
             if (s.length() == 2)
                 return true;
             return isPalindrome(s.substring(1, s.length() - 1));
         }
         return false;
+    }
+
+    private static String findLongestString(Set<String> set) {
+        if (set.isEmpty()) {
+            return null; // Если множество пустое, возвращаем null
+        }
+        
+        String longest = "";
+        
+        for (String s : set) {
+            if (s.length() > longest.length()) {
+                longest = s;
+            }
+        }
+        
+        return longest;
     }
 }
