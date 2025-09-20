ff500a363f7d077e24d13f0f79672fb182de6f47
Victor <devitos76@gmial.com>
2025-09-20 17:35:25 +0300

    fix main

--- src/main/java/org/future/lkapp/Lk.java
+++ src/main/java/org/future/lkapp/Lk.java
@@ -35,12 +35,12 @@ public class Lk {
 
         ListNode l1 = new ListNode(1);
         ListNode l2 = new ListNode(2);
-        // ListNode l3 = new ListNode(3);
-        // ListNode l4 = new ListNode(4);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
 
         l1.next = l2;
-        // l2.next = l3;
-        // l3.next = l4;
+        l2.next = l3;
+        l3.next = l4;
         
         Remnnode rn = new Remnnode();
         NodeService ns = new NodeService();
