deb81abd8e55cdf760c40b55c8796342901820f4
Victor <devitos76@gmial.com>
2025-10-05 14:15:00 +0300

    rename class

--- src/main/java/org/future/lkapp/Solutions/balanced_binary_tree/Bbt.java
+++ src/main/java/org/future/lkapp/Solutions/balanced_binary_tree/Bbt.java
@@ -2,17 +2,27 @@ package org.future.lkapp.Solutions.balanced_binary_tree;
 
 import org.future.lkapp.Solutions.sametree.TreeNode;
 
-public class bbt {
+public class Bbt {
+
+    private int right_counter;
+    private int left_counter; 
+
     public boolean isBalanced(TreeNode root) {
-        
+        goDig(root);
         return false;
     }
 
     public void goDig(TreeNode node) {
-        if (node == null) return;
+        if (node == null)
+            return;
         System.out.println(node.value);
         goDig(node.right);
         goDig(node.left);
+        right_counter++;
+    }
+
+    public int getRightCounter(){
+        return right_counter;
     }
 
 }
